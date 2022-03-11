bf6afc825c1699d439caf8aee22516fd4af1f604
Shri-Dev123 <100713879+Shri-Dev123@users.noreply.github.com>
2022-03-11 15:56:20 +0530

    Add: addding functionality

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
+const bananaInput = document.querySelector(".section-inputbox");
+const translateButton = document.querySelector(".translate-button");
+let tranlatedTextArea = document.querySelector(".translated-textarea");
 
+var serverUrl = "https://api.funtranslations.com/translate/minion.json";
+
+var urlEdit = (text) => {
+  return `${serverUrl}?text=${text}`;
+};
+
+var errorHandler = (error) => {
+  console.log(`${error}, something went wrong...! please try again later`);
+};
+
+var getFetchedData = () => {
+  var inputText = bananaInput.value;
+  if (inputText === "") {
+    tranlatedTextArea.value = "Please type something..! to Translate";
+  } else if (inputText !== "") {
+    return fetch(urlEdit(inputText))
+      .then((response) => response.json())
+      .then((json) => (tranlatedTextArea.value = json.contents.translated))
+      .catch((error) => alert(errorHandler()));
+  }
+};
+
+translateButton.addEventListener("click", getFetchedData);
